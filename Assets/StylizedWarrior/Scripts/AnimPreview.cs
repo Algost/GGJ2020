2c950e53fa36562bf5b467e216daca590d68f692
Nicolas Constanty <nicolas.constanty@epitech.eu>
2020-02-01 20:30:18 +0100

    Add new terrain

---
+++ Assets/StylizedWarrior/Scripts/AnimPreview.cs
@@ -0,0 +1,231 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimPreview : MonoBehaviour
+{
+    public Animator anim;
+    /*
+    private float inputH;
+    private float inputV;
+    private bool run;
+    */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        //run = false;
+    }
+
+    public void theIdle()
+    {
+        //anim.SetTrigger("theIdle");
+        anim.Play("Idle", -1, 0f);
+
+    }
+
+    public void MeleeHitReact()
+    {
+        anim.Play("Melee_Hit_React", -1, 0f);
+    }
+
+    public void Jump()
+    {
+        anim.Play("Jump", -1, 0f);
+    }
+    public void JumpStart()
+    {
+        anim.Play("Jump_Start", -1, 0f);
+    }
+    public void Fall()
+    {
+        anim.Play("Fall", -1, 0f);
+    }
+    public void Land()
+    {
+        anim.Play("Land", -1, 0f);
+    }
+       
+
+
+    public void Sit_GetUp()
+    {
+        anim.Play("Sit_getUP", -1, 0f);
+    }
+
+    public void Sit_SitDown()
+    {
+        anim.Play("Sit_down", -1, 0f);
+    }
+
+    public void Sit_Sitting()
+    {
+        anim.Play("Sitting", -1, 0f);
+    }
+
+
+    public void Weapon_Idle()
+    {
+        anim.Play("Idle1(Weapon)",-1,0f);
+    }
+
+    public void Weapon_Idle2()
+    {
+        anim.Play("Idle2(Weapon)", -1, 0f);
+    }
+    public void Weapon_Attack1()
+    {
+        anim.Play("Attack1", -1, 0f);
+    }
+    public void Weapon_Attack2()
+    {
+        anim.Play("Attack2", -1, 0f);
+    }
+    public void Weapon_Attack3()
+    {
+        anim.Play("Attack3", -1, 0f);
+    }
+    public void Weapon_Attack4()
+    {
+        anim.Play("Attack4", -1, 0f);
+    }
+    public void Kick1()
+    {
+        anim.Play("Kick1", -1, 0f);
+    }
+    public void Kick2()
+    {
+        anim.Play("Kick2", -1, 0f);
+    }
+
+    public void Walk()
+    {
+        anim.Play("Walk", -1, 0f);
+    }
+    public void Run()
+    {
+        anim.Play("Run", -1, 0f);
+    }
+    public void IdleLeanOnWall()
+    {
+        anim.Play("Idle(LeanOnWall)", -1, 0f);
+    }
+
+    public void WalkWeapon()
+    {
+        anim.Play("Walk_Weapon", -1, 0f);
+    }
+    public void RunWeapon()
+    {
+        anim.Play("Run_Weapon", -1, 0f);
+    }
+    public void DodgeL()
+    {
+        anim.Play("Dodge_Left", -1, 0f);
+    }
+    public void DodgeR()
+    {
+        anim.Play("Dodge_Right", -1, 0f);
+    }
+
+
+    public void Stunned()
+    {
+        anim.Play("Stunned", -1, 0f);
+    }
+    public void HitReactWeapon()
+    {
+        anim.Play("HitReactWeapon", -1, 0f);
+    }
+    public void Spawn()
+    {
+        anim.Play("Spawn", -1, 0f);
+    }
+
+    public void GetUp()
+    {
+        anim.Play("Get UP (Knocked Out)", -1, 0f);
+    }
+    public void Death()
+    {
+        anim.Play("Death", -1, 0f);
+    }
+    public void KnockedOut()
+    {
+        anim.Play("Knocked_Out", -1, 0f);
+    }
+    public void Revive()
+    {
+        anim.Play("Revive", -1, 0f);
+    }
+    public void SleepStart()
+    {
+        anim.Play("Sleep_Start", -1, 0f);
+    }
+    public void Sleeping()
+    {
+        anim.Play("Sleeping", -1, 0f);
+    }
+    public void SleepGetUp()
+    {
+        anim.Play("Sleep_getUP", -1, 0f);
+    }
+
+    public void Wave()
+    {
+        anim.Play("Wave", -1, 0f);
+    }
+    public void Taunt()
+    {
+        anim.Play("Taunt", -1, 0f);
+    }
+    public void Dance()
+    {
+        anim.Play("Dance", -1, 0f);
+    }
+    public void Pushup()
+    {
+        anim.Play("Pushup", -1, 0f);
+    }
+    public void ClimbRM()
+    {
+        anim.Play("Climb_RM", -1, 0f);
+    }
+    public void Climb()
+    {
+        anim.Play("Climb", -1, 0f);
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        /**
+        if(Input.GetMouseButtonDown(1))
+        {
+            anim.Play("Melee_Hit_React", -1, 0f);
+            print("I clicked");
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            run = true;
+        }
+        else
+        {
+            run = false;
+        }
+
+        inputH = Input.GetAxis("Horizontal");
+        inputV = Input.GetAxis("Vertical");
+
+        anim.SetFloat("inputH", inputH);
+        anim.SetFloat("inputV", inputV);
+        anim.SetBool("run", run);
+    */
+
+
+    }
+}
---
+++ Assets/StylizedWarrior/Scripts/CameraController.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+
+    //public Camera GetComponent<Camera>();
+    //public Camera GetComponent<Camera2>();
+
+    //Camera cam01 = GameObject.GetComponent<Camera>();
+    //Camera cam02 = GameObject.GetComponent<Camera2>();
+
+    public Camera Camera1;
+    public Camera Camera2;
+    public Camera Camera3;
+    public Camera Camera4;
+    void Start()
+    {
+        //GetComponent<Camera>().enabled = true;
+        //GetComponent<Camera2>().enabled = false;
+        Camera1.enabled = true;
+        Camera2.enabled = false;
+        Camera3.enabled = false;
+        Camera4.enabled = false;
+    }
+
+    void Update()
+    {
+        //This will toggle the enabled state of the two cameras between true and false each time
+        /**if (Input.GetKeyUp(KeyCode.Space))
+        {
+            GetComponent<Camera>().enabled = !GetComponent<Camera>().enabled;
+            GetComponent<Camera2>().enabled = GetComponent<Camera2>().enabled;
+        }
+        */
+
+        /**if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Camera1.enabled = false;
+            Camera2.enabled = true;
+        }
+        */
+
+    
+
+    }
+
+    public void Camera1to2()
+    {
+        Camera1.enabled = false;
+        Camera2.enabled = true;
+    }
+
+    public void Camera2to3()
+    {
+        Camera2.enabled = false;
+        Camera3.enabled = true;
+    }
+
+    public void Camera2to1()
+    {
+        Camera2.enabled = false;
+        Camera1.enabled = true;
+    }
+
+    public void Camera3to2()
+    {
+        Camera3.enabled = false;
+        Camera2.enabled = true;
+    }
+
+    public void Camera3to4()
+    {
+        Camera3.enabled = false;
+        Camera4.enabled = true;
+    
+    }
+    public void Camera4to3()
+    {
+        Camera4.enabled = false;
+        Camera3.enabled = true;
+    }
+
+    public void Camera4to1()
+    {
+        Camera4.enabled = false;
+        Camera1.enabled = true;
+    }
+    public void Camera1to4()
+    {
+        Camera1.enabled = false;
+        Camera4.enabled = true;
+    }
+
+
+}
