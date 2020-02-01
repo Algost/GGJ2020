968405308f7d7bce740d60774d240758b8bdb0ca
Nicolas Constanty <nicolas.constanty@epitech.eu>
2020-02-01 13:05:47 +0100

    Repair v1

--- Assets/02_Scripts/Repairable.cs
+++ Assets/02_Scripts/Repairable.cs
@@ -7,9 +7,18 @@ using UnityEngine.InputSystem;
 public interface IRepairable
 {
     int repairActionsCount { get; }
+    InputActionMap possibleActions { get; }
+    float actionTime { get; }
+
     void StartRepair();
-    InputAction GetNextAction();
+    void FailRepair();
+    void SuccessRepair();
     void FinishRepair();
+
+    InputAction GetNextAction();
+    void CreateNewAction();
+
+    void InitActions();
 }
 
 public class Repairable : MonoBehaviour, IRepairable
@@ -25,20 +34,18 @@ public class Repairable : MonoBehaviour, IRepairable
     public UnityEvent onFinishRepair;
     [SerializeField]
     private int m_repairCount;
+    [SerializeField]
+    private float m_actionTime;
 
     public int repairActionsCount => m_repairCount;
 
-    private void InitActions()
-    {
-        m_repairActionsQueue.Clear();
-        for (int i = 0; i < repairActionsCount; i++)
-        {
-            m_repairActionsQueue.Enqueue(GetRandomAction());
-        }
-    }
+    InputActionMap IRepairable.possibleActions => m_possibleActions;
+
+    public float actionTime => m_actionTime;
 
     private void Start()
     {
+        InitActions();
         onOverHeat.Invoke();
     }
 
@@ -55,12 +62,36 @@ public class Repairable : MonoBehaviour, IRepairable
 
     public InputAction GetNextAction()
     {
-        m_repairActionsQueue.Enqueue(GetRandomAction());
+        if (m_repairActionsQueue.Count == 0) return null;
         return m_repairActionsQueue.Dequeue();
     }
 
     public void FinishRepair()
     {
         onFinishRepair.Invoke();
+    }
+
+    public void FailRepair()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void SuccessRepair()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void CreateNewAction()
+    {
+        m_repairActionsQueue.Enqueue(GetRandomAction());
+    }
+
+    void InitActions()
+    {
+        m_repairActionsQueue.Clear();
+        for (int i = 0; i < repairActionsCount; i++)
+        {
+            m_repairActionsQueue.Enqueue(GetRandomAction());
+        }
     }
 }
